7f731cd783bc4a75d65ad51b47386805d53bab5a
derui <derutakayu@gmail.com>
2016-10-16 07:57:12 +0900

    Change Zsh's main keybinding to vi

--- dot.zsh/20_bindings.zsh
+++ dot.zsh/20_bindings.zsh
@@ -1,47 +1,85 @@
 # -*- mode:shell-script -*-
 
 # Emacs style key binding
-bindkey -e
+bindkey -v
+
+bindkey -M viins 'jj' vi-cmd-mode
+
+# Add emacs-like keyvind to viins mode
+bindkey -M viins '^F'    forward-char
+bindkey -M viins '^B'    backward-char
+bindkey -M viins '^P'    up-line-or-history
+bindkey -M viins '^N'    down-line-or-history
+bindkey -M viins '^A'    beginning-of-line
+bindkey -M viins '^E'    end-of-line
+bindkey -M viins '^K'    kill-line
+bindkey -M viins '^Y'    yank
+bindkey -M viins '^W'    backward-kill-word
+bindkey -M viins '^U'    backward-kill-line
+bindkey -M viins '^H'    backward-delete-char
+bindkey -M viins '^?'    backward-delete-char
+bindkey -M viins '^G'    send-break
+bindkey -M viins '^D'    delete-char-or-list
+
+bindkey -M vicmd '^A'    beginning-of-line
+bindkey -M vicmd '^E'    end-of-line
+bindkey -M vicmd '^K'    kill-line
+bindkey -M vicmd '^Y'    yank
+bindkey -M vicmd '^W'    backward-kill-word
+bindkey -M vicmd '^U'    backward-kill-line
+bindkey -M vicmd '/'     vi-history-search-forward
+bindkey -M vicmd '?' vi-history-search-backward
+
+bindkey -M vicmd 'gg' beginning-of-line
+bindkey -M vicmd 'G' end-of-line
 
 # エディタでコマンドラインを編集できる。
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey "^O" edit-command-line
-bindkey "^V" quoted-insert
+bindkey -M vicmd "^O" edit-command-line
+bindkey -M viins "^V" quoted-insert
 
 # 履歴検索を有効にする。
-# C-p 履歴の後方検索
-# C-n 履歴の前方検索
 autoload history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 
-bindkey -M emacs '^P' history-substring-search-up
-bindkey -M emacs '^N' history-substring-search-down
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
+
+bindkey -M vicmd 'j' history-substring-search-up
+bindkey -M vicmd 'k' history-substring-search-down
+
+# Surround a forward word by single quote
+_quote-previous-word-in-single() {
+    modify-current-argument '${(qq)${(Q)ARG}}'
+    zle vi-forward-blank-word
+}
+zle -N _quote-previous-word-in-single
+bindkey -M viins '^Q' quote-previous-word-in-single
+
+# Surround a forward word by double quote
+_quote-previous-word-in-double() {
+    modify-current-argument '${(qqq)${(Q)ARG}}'
+    zle vi-forward-blank-word
+}
+zle -N _quote-previous-word-in-double
+bindkey -M viins '^Xq' _quote-previous-word-in-double
 
 # pecoで検索する
-function my-peco-select-history() {
+function _peco-select-history() {
 
-    local tac
-    if [[ $(which tac) > /dev/null ]]; then
-        tac="tac"
-    else
-        tac="tail -r"
-    fi
-
-    BUFFER=$(history -n 1 | \
-        eval $tac | \
-        peco --query "$LBUFFER")
+    BUFFER=$(history -nr 1 | peco --query "$LBUFFER")
     CURSOR=$#BUFFER
     zle clear-screen
 }
 
-zle -N my-peco-select-history
-bindkey "^r" my-peco-select-history
+zle -N _peco-select-history
+bindkey "^r" _peco-select-history
 
 # 一つ上のディレクトリに上がる
 # http://shakenbu.org/yanagi/d/?date=20120301
-my-cdup() {
+_cdup() {
     if [ -z "$BUFFER" ]; then
         echo
         cd ..
@@ -50,8 +88,8 @@ my-cdup() {
         zle self-insert '^'
     fi
 }
-zle -N my-cdup
-bindkey '\^' my-cdup
+zle -N _cdup
+bindkey '\^' _cdup
 
 # ディレクトリを移動する度にlsを実行する
 chpwd() {
--- dot.zsh/30_prompt.zsh
+++ dot.zsh/30_prompt.zsh
@@ -17,6 +17,30 @@ left_down_prompt_preexec() {
     print -rn -- $terminfo[el]
 }
 
+add-zsh-hook preexec left_down_prompt_preexec
+function zle-keymap-select zle-line-init zle-line-finish {
+    case $KEYMAP in
+        main|viins)
+            PROMPT_2="$fg[cyan]-- INSERT --$reset_color"
+            ;;
+        vicmd)
+            PROMPT_2="$fg[white]-- NORMAL --$reset_color"
+            ;;
+        vivis|vivli)
+            PROMPT_2="$fg[yellow]-- VISUAL --$reset_color"
+            ;;
+        virep)
+            PROMPT_2="$fg[red]-- REPLACE --$reset_color"
+            ;;
+    esac
+    PROMPT="%{$terminfo_down_sc$PROMPT_2$terminfo[rc]%}[%(?.%{${fg[green]}%}.%{${fg[red]}%})${HOST}%{${reset_color}%}]%# "
+    zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+zle -N edit-command-line
+
 PROMPT="%{$terminfo_down_sc$PROMPT_2$terminfo[rc]%}[%(?.%{${fg[green]}%}.%{${fg[red]}%})${HOST}%{${reset_color}%}]%# "
 
 # Right PROMPT

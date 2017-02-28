b6f4857cc5ea5675b0c296a38cdd9e8f4fa76f1c
derui <derutakayu@gmail.com>
2017-02-28 14:20:33 +0900

    Fix awesome settings for 4.x framework

--- configurations/awesome/rc.lua
+++ configurations/awesome/rc.lua
@@ -271,49 +271,32 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "e", function () awful.screen.focus( 2) end),
     -- simulate tag swap...
     awful.key({ modkey,           }, "s",
-        function ()
-	    local current_screen = mouse.screen
-	    local target_screen = 0
-	    if current_screen == 1 then
-	        target_screen = 2
-            else
-	        target_screen = 1
-	    end
-	    local current_tag = nil
-	    local target_tag = nil
-	    for i, tag in ipairs(tags[current_screen]) do
-	        if tag.selected then
-		    current_tag = i
-		    break
-		end
-            end
+       function ()
+          local current_screen = mouse.screen
+          local target_screen = 0
 
-	    for i, tag in ipairs(tags[target_screen]) do
-	        if tag.selected then
-		    target_tag = i
-		    break
-		end
-            end
-	    local cclients = tags[current_screen][current_tag]:clients()
-	    local tclients = tags[target_screen][target_tag]:clients()
-	    for i, c in ipairs(cclients) do
-	        awful.client.movetoscreen(c, target_screen)
-            end
+          if screen.count() == 1 then
+             return
+          end
 
-	    for i, c in ipairs(tclients) do
-	        awful.client.movetoscreen(c, current_screen)
-	    end
+          if current_screen.index == 1 then
+             target_screen = 2
+          else
+             target_screen = 1
+          end
 
-	    awful.screen.focus(target_screen)
-	end),
+          current_screen:swap(screen[target_screen])
+
+          awful.screen.focus(screen[target_screen])
+    end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
     awful.key({ modkey,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end),
+       function ()
+          awful.client.focus.history.previous()
+          if client.focus then
+             client.focus:raise()
+          end
+    end),
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
@@ -334,45 +317,45 @@ globalkeys = awful.util.table.join(
     -- Prompt
     --awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
     awful.key({ modkey },            "r",
-		    function ()
-		    awful.util.spawn("dmenu_run -i -p 'Run command:' -nb '" ..
-			    beautiful.bg_normal .. "' -nf '" .. beautiful.fg_normal ..
-			    "' -sb '" .. beautiful.bg_focus ..
-			    "' -sf '" .. beautiful.fg_focus .. "'" ..
-			    " -fn '-*-Fixed-Bold-R-Normal-*-16-*-*-*-*-*-*-*'")
-		    end),
+       function ()
+          awful.util.spawn("dmenu_run -i -p 'Run command:' -nb '" ..
+                              beautiful.bg_normal .. "' -nf '" .. beautiful.fg_normal ..
+                              "' -sb '" .. beautiful.bg_focus ..
+                              "' -sf '" .. beautiful.fg_focus .. "'" ..
+                              " -fn '-*-Fixed-Bold-R-Normal-*-16-*-*-*-*-*-*-*'")
+    end),
 
     awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run({ prompt = "Run Lua code: " },
-                  mypromptbox[mouse.screen].widget,
-                  awful.util.eval, nil,
-                  awful.util.getdir("cache") .. "/history_eval")
-              end)
+       function ()
+          awful.prompt.run({ prompt = "Run Lua code: " },
+             mypromptbox[mouse.screen].widget,
+             awful.util.eval, nil,
+             awful.util.getdir("cache") .. "/history_eval")
+    end)
 )
 
 clientkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
-    awful.key({ modkey }, "space",  awful.client.floating.toggle                     ),
-    awful.key({ modkey }, "Next",   function() awful.client.moveresize(20, 20, -40, -40) end),
-    awful.key({ modkey }, "Prior",   function() awful.client.moveresize(20, 20, -40, -40) end),
-    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
-    awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
-    awful.key({ modkey, "Shift"   }, "r",      function (c) c:redraw()                       end),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
-    awful.key({ modkey,           }, "n",
-        function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end),
-    awful.key({ modkey,           }, "m",
-        function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c.maximized_vertical   = not c.maximized_vertical
-        end)
+   awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
+   awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
+   awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
+   awful.key({ modkey }, "space",  awful.client.floating.toggle                     ),
+   awful.key({ modkey }, "Next",   function() awful.client.moveresize(20, 20, -40, -40) end),
+   awful.key({ modkey }, "Prior",   function() awful.client.moveresize(20, 20, -40, -40) end),
+   awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
+   awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
+   awful.key({ modkey, "Shift"   }, "r",      function (c) c:redraw()                       end),
+   awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
+   awful.key({ modkey,           }, "n",
+      function (c)
+         -- The client currently has the input focus, so it cannot be
+         -- minimized, since minimized clients can't have the focus.
+         c.minimized = true
+   end),
+   awful.key({ modkey,           }, "m",
+      function (c)
+         c.maximized_horizontal = not c.maximized_horizontal
+         c.maximized_vertical   = not c.maximized_vertical
+   end)
 )
 
 -- Compute the maximum number of digit we need, limited to 9
@@ -385,39 +368,42 @@ end
 -- Be careful: we use keycodes to make it works on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, keynumber do
-    globalkeys = awful.util.table.join(globalkeys,
-        awful.key({ modkey }, "#" .. i + 9,
-                  function ()
-                        local screen = mouse.screen
-                        if tags[screen][i] then
-                            awful.tag.viewonly(tags[screen][i])
-                        end
-                  end),
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = mouse.screen
-                      if tags[screen][i] then
-                          awful.tag.viewtoggle(tags[screen][i])
-                      end
-                  end),
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus and tags[client.focus.screen][i] then
-                          awful.client.movetotag(tags[client.focus.screen][i])
-                      end
-                  end),
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus and tags[client.focus.screen][i] then
-                          awful.client.toggletag(tags[client.focus.screen][i])
-                      end
-                  end))
+   globalkeys = awful.util.table.join(
+      globalkeys,
+      awful.key({ modkey }, "#" .. i + 9,
+         function ()
+            local screen = mouse.screen.index
+            if tags[screen][i] then
+               awful.tag.viewonly(tags[screen][i])
+            end
+      end),
+      awful.key({ modkey, "Control" }, "#" .. i + 9,
+         function ()
+            local screen = mouse.screen.index
+            if tags[screen][i] then
+               awful.tag.viewtoggle(tags[screen][i])
+            end
+      end),
+      awful.key({ modkey, "Shift" }, "#" .. i + 9,
+         function ()
+            local screen = client.focus.screen.index
+            if client.focus and tags[screen][i] then
+               awful.client.movetotag(tags[screen][i])
+            end
+      end),
+      awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
+         function ()
+            local screen = client.focus.screen.index
+            if client.focus and tags[screen][i] then
+               awful.client.toggletag(tags[screen][i])
+            end
+   end))
 end
 
 clientbuttons = awful.util.table.join(
-    awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-    awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+   awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
+   awful.button({ modkey }, 1, awful.mouse.client.move),
+   awful.button({ modkey }, 3, awful.mouse.client.resize))
 
 -- Set keys
 root.keys(globalkeys)
@@ -425,47 +411,47 @@ root.keys(globalkeys)
 
 -- {{{ Rules
 awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
+   -- All clients will match this rule.
+   { rule = { },
+     properties = { border_width = beautiful.border_width,
+                    border_color = beautiful.border_normal,
+                    focus = true,
+                    keys = clientkeys,
+                    buttons = clientbuttons } },
+   { rule = { class = "MPlayer" },
+     properties = { floating = true } },
+   { rule = { class = "pinentry" },
+     properties = { floating = true } },
+   { rule = { class = "gimp" },
+     properties = { floating = true } },
 }
 -- }}}
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c, startup)
-    -- Add a titlebar
-    -- awful.titlebar.add(c, { modkey = modkey })
+                         -- Add a titlebar
+                         -- awful.titlebar.add(c, { modkey = modkey })
 
-    -- Enable sloppy focus
-    c:connect_signal("mouse::enter", function(c)
-        if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-            and awful.client.focus.filter(c) then
-            client.focus = c
-        end
-    end)
+                         -- Enable sloppy focus
+                         c:connect_signal("mouse::enter", function(c)
+                                             if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
+                                             and awful.client.focus.filter(c) then
+                                                client.focus = c
+                                             end
+                         end)
 
-    if not startup then
-        -- Set the windows at the slave,
-        -- i.e. put it at the end of others instead of setting it master.
-        -- awful.client.setslave(c)
+                         if not startup then
+                            -- Set the windows at the slave,
+                            -- i.e. put it at the end of others instead of setting it master.
+                            -- awful.client.setslave(c)
 
-        -- Put windows in a smart way, only if they does not set an initial position.
-        if not c.size_hints.user_position and not c.size_hints.program_position then
-            awful.placement.no_overlap(c)
-            awful.placement.no_offscreen(c)
-        end
-    end
+                            -- Put windows in a smart way, only if they does not set an initial position.
+                            if not c.size_hints.user_position and not c.size_hints.program_position then
+                               awful.placement.no_overlap(c)
+                               awful.placement.no_offscreen(c)
+                            end
+                         end
 end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)

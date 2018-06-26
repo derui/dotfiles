74036e57c0635b99639ffe43af701c39ddd018a4
derui <derutakayu@gmail.com>
2018-06-26 23:57:25 +0900

    fix: launch bar for monitor2 if it exists

--- polybar-launch.sh
+++ polybar-launch.sh
@@ -5,6 +5,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 polybar monitor1 &
-polybar monitor2 &
+
+
+if [[ -x `which xrandr` && `xrandr -q | grep " connected" | wc -l` -eq "2" ]]; then
+    polybar monitor2 &
+fi
 
 echo "Bars launched"

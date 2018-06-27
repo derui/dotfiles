caf89166ec2cb5781513488442e2680e43e9b9af
derui <derutakayu@gmail.com>
2018-06-27 09:25:05 +0900

    fix: support MONITOR environment variable

--- polybar-launch.sh
+++ polybar-launch.sh
@@ -4,10 +4,11 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
+MONITOR=$(polybar -m | head -1 | sed -e '/:.*$//g')
 polybar monitor1 &
 
-
 if [[ -x `which xrandr` && `xrandr -q | grep " connected" | wc -l` -eq "2" ]]; then
+    MONITOR=$(polybar -m | tail -1 | sed -e '/:.*$//g')
     polybar monitor2 &
 fi
 

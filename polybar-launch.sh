b338a7b5cd03c5a0a5986593f85869dffe52b48d
derui <derutakayu@gmail.com>
2019-03-22 14:39:23 +0900

    fix: polybar config

--- polybar-launch.sh
+++ polybar-launch.sh
@@ -4,7 +4,7 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-MONITOR=$(polybar -m | head -1 | sed -e '/:.*$//g')
+export MONITOR=$(polybar -m | head -1 | sed -e 's/:.*$//g')
 polybar monitor1 &
 
 if [[ -x `which xrandr` && `xrandr -q | grep " connected" | wc -l` -eq "2" ]]; then

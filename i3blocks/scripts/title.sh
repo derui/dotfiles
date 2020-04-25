e1565048d3ac7d9b55ce582145dd5ece23bd4f43
derui <derutakayu@gmail.com>
2020-04-25 10:44:34 +0900

    fix: do not echo same title twice

--- i3blocks/scripts/title.sh
+++ i3blocks/scripts/title.sh
@@ -5,8 +5,12 @@ function main() {
         exit 1
     fi
 
+    local current_title=""
     while read xtit; do
-        echo $xtit
+        if [[ "$current_title" != "$xtit" ]]; then
+            echo $xtit
+            current_title=$xtit
+        fi
     done < <(xtitle -s)
 }
 

421ccc3d41466dc9feb483bc8c9a2d32ed15eab2
derui <derutakayu@gmail.com>
2021-01-22 17:27:08 +0900

    fix: syntax error

--- bin/use-jack.sh
+++ bin/use-jack.sh
@@ -9,9 +9,10 @@ DEFAULT_PROFILE="output:analog-stereo+input:analog-stereo"
 CARD_NAME=usb-Yamaha_Corporation_Steinberg_UR22C-00
 DEFAULT_CARD=alsa_card.$CARD_NAME
 
-# turn off target device in pulseaudio
+echo "turn off target device in pulseaudio"
 pactl set-card-profile $DEFAULT_CARD off
 
+echo "Starting jack..."
 jack_control start
 if [[ $? != 0 ]]; then
     echo "Jack startup failed"
@@ -32,21 +33,21 @@ while read -r v; do
     break
 done
 
+echo "Exiting jack..."
 jack_control exit
-echo "Jack exited"
 
-# restore default profile
+echo "Restore default profile"
 pactl set-card-profile $DEFAULT_CARD $DEFAULT_PROFILE
 
 while true; do
     echo "Waiting to finish activation on pulseaudio..."
-    local sinks=$(pacmd list-sinks | rg "name:" | rg -n "$CARD_NAME")
-    if [[ sinks >= 1 ]]; then
+    sinks=$(pacmd list-sinks | rg "name:" | rg -c "$CARD_NAME")
+    if [[ $sinks -ge 1 ]]; then
         break
     fi
 
     sleep 1
 done
 
-pacmd set-default-sink alsa_output.$CARD_NAME.analog-stereo
-pacmd set-default-source alsa_input.$CARD_NAME.analog-stereo
+pacmd set-default-sink "alsa_output.$CARD_NAME.analog-stereo"
+pacmd set-default-source "alsa_input.$CARD_NAME.analog-stereo"

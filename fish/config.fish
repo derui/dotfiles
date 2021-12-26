d7e99589b8e1d2fc7f4c9959efb44bac802068b4
derui <derutakayu@gmail.com>
2021-12-26 12:56:26 +0900

    fix: avoid endless loop

--- fish/config.fish
+++ fish/config.fish
@@ -27,7 +27,6 @@ end
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
 end
 
 # change location of packages installed by fisher

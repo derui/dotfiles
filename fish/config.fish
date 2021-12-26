a61dbdcdfb5903e49a93c07f342f7a6e478ef844
derui <derutakayu@gmail.com>
2021-12-26 13:00:42 +0900

    fix: installation command for fisher

--- fish/config.fish
+++ fish/config.fish
@@ -26,7 +26,7 @@ end
 # install fisher
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 end
 
 # change location of packages installed by fisher

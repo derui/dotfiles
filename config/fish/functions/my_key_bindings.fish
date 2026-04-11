b524348d6e82b7fa2c0fa0131a0e662bf9b1fc09
derui <derutakayu@gmail.com>
2026-04-11 12:55:40 +0900

    fix: invalid sequence

--- config/fish/functions/my_key_bindings.fish
+++ config/fish/functions/my_key_bindings.fish
@@ -6,7 +6,7 @@ function my_key_bindings
 
     bind -M default \cr 'history save; history merge; __select_history'
     bind -M insert \cr 'history save; history merge; __select_history'
-    bind -M default \cs__select_ghq_cd
+    bind -M default \cs __select_ghq_cd
     bind -M insert \cs __select_ghq_cd
     bind -M insert \c_ __integrated_completer
     bind -M default \cgs __git_change_branch

fd850bba2737a9193c7823cc46413890e8ce9e5b
derui <derutakayu@gmail.com>
2026-07-18 18:26:47 +0900

    feat: Add claude executable path to fish config
    Adds the claude code executable path to the fish
    configuration when running within a nix-managed environment.

--- config/fish/conf.d/nix.fish
+++ config/fish/conf.d/nix.fish
@@ -1,4 +1,7 @@
 
 if test -e $HOME/.nix-profile/etc/profile.d/nix.fish
     . $HOME/.nix-profile/etc/profile.d/nix.fish
+
+    # nixos用の設定を追加しておく
+    set -xg CLAUDE_CODE_EXECUTABLE (which claude)
 end
